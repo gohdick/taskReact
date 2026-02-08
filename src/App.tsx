da307499f39fa31b3e66c542599f82ffd0cf4a4f
gohdick <thirasaksimtong@gmail.com>
2026-02-08 13:23:06 +0700

    npm zustand

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-emerald-400">
-        Tailwind ใช้งานได้แล้ว 🚀
-      </h1>
+      <div className="p-6">
+      <h1 className="text-2xl font-bold">Task Manager</h1>
     </div>
     </>
   )
